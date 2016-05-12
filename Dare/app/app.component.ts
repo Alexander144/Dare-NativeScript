573a1826f30638218dcffb3453b2fc2ab8d8afc6
Alexander Larsen <Alexan2405@gmail.com>
2016-05-12 14:19:45 +0200

    Starter-Content

---
+++ Dare/app/app.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  template: 
+
+  "<StackLayout> <Label text='Dare You'> </Label> <Button text='tap'> </Button> </StackLayout>",
+})
+export class AppComponent {}
