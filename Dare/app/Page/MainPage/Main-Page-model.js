ca9ec57b791c5689d911bbcece9b5428b1f7a2db
Alexander Larsen <Alexan2405@gmail.com>
2016-08-16 00:03:54 +0200

    Endelig klart det (Y) (Y)

--- Dare/app/Page/MainPage/Main-Page-model.js
+++ Dare/app/Page/MainPage/Main-Page-model.js
@@ -4,28 +4,36 @@ var observable_array_1 = require("data/observable-array");
 var firebase = require("nativescript-plugin-firebase");
 var Dare_1 = require("../Class/Dare/Dare");
 var M_Dare;
+var self;
 var MainModel = (function (_super) {
     __extends(MainModel, _super);
     function MainModel() {
         _super.call(this);
         this.Dares = new observable_array_1.ObservableArray();
+        this.m_Dare = "læl";
+        this.set("m_Dare", "lol");
         this.GetDares();
+        self = this;
     }
-    MainModel.prototype.onChildEvent = function (result) {
-        if (result.type === "ChildAdded" && result.type != "undefined") {
-            console.log("Event type: " + result.type);
-            console.log("Key: " + result.key);
-            console.log("Value: " + JSON.stringify(result.value.Dare));
-            this.m_Dare = JSON.stringify(result.value.Dare);
-        }
-    };
     MainModel.prototype.GetDares = function () {
+        var onChildEvent = function (result) {
+            if (result.type === "ChildAdded" && result.type != "undefined") {
+                console.log("Event type: " + result.type);
+                console.log("Key: " + result.key);
+                console.log("Value: " + JSON.stringify(result.value.Dare));
+                alert(result.key + JSON.stringify(result.value.Dare) + JSON.stringify(result.value.From));
+                self.newDare(result.key, result.value.Dare, result.value.From);
+                //self.Dares.push(new Dare("lol","2323","2323"));
+                self.set("m_Dare", "lol");
+            }
+        };
         // listen to changes in the /users path
-        firebase.addChildEventListener(this.onChildEvent, "/Dares/Lol12345");
-        this.set('M_Dare', M_Dare);
-        console.log("This is::::::" + M_Dare);
-        this.Dares.push(new Dare_1.default("1", "lol", "leel"));
+        firebase.addChildEventListener(onChildEvent, "/Dares/Alexander144");
+        //self.Dares.push(new Dare("1","lol","leel"));
         //this.Dares.push(new Dare("12","Eat", this.User));
+    };
+    MainModel.prototype.newDare = function (id, nDare, From) {
+        this.Dares.push(new Dare_1.default(id, nDare, From));
     };
     MainModel.prototype.Send = function () {
         console.debug("Send");
--- Dare/platforms/ios/Dare/app/Page/MainPage/Main-Page-model.ts
+++ Dare/platforms/ios/Dare/app/Page/MainPage/Main-Page-model.ts
@@ -3,6 +3,8 @@ import { ObservableArray } from "data/observable-array";
 import firebase = require("nativescript-plugin-firebase");
 import Dare from "../Class/Dare/Dare";
 var M_Dare;
+
+ var self;
 class MainModel extends Observable{
     Dares: ObservableArray<Dare>;
     User: string;
@@ -14,30 +16,33 @@ class MainModel extends Observable{
     constructor(){
         super();
         this.Dares = new ObservableArray<Dare>();
-       
+        this.m_Dare = "læl";
+        this.set("m_Dare","lol");
         this.GetDares();
+        self = this;
     }
-    onChildEvent(result:any) {
-
-      
+  
+    GetDares(){
+        
+  var onChildEvent = function(result:any) {
          if (result.type === "ChildAdded" && result.type != "undefined") {
                console.log("Event type: " + result.type);
                 console.log("Key: " + result.key);
                 console.log("Value: " + JSON.stringify(result.value.Dare));
-            this.m_Dare = JSON.stringify(result.value.Dare);
-
+                alert(result.key + JSON.stringify(result.value.Dare) + JSON.stringify(result.value.From));
+            self.newDare(result.key, result.value.Dare, result.value.From);
+            //self.Dares.push(new Dare("lol","2323","2323"));
+            self.set("m_Dare","lol");
          }
      }
-    GetDares(){
-        
-
     // listen to changes in the /users path
-    firebase.addChildEventListener(this.onChildEvent, "/Dares/Lol12345");
-    this.set('M_Dare',M_Dare);
-    console.log("This is::::::" + M_Dare);
-    
-     this.Dares.push(new Dare("1","lol","leel"));
+    firebase.addChildEventListener(onChildEvent, "/Dares/Alexander144");
+  
+     //self.Dares.push(new Dare("1","lol","leel"));
         //this.Dares.push(new Dare("12","Eat", this.User));
+    }
+    newDare(id:string, nDare:string, From:string){
+        this.Dares.push(new Dare(id,nDare,From));
     }
     Send(){
         console.debug("Send");
